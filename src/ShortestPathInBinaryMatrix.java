fd39b8c679d3109455cfe2cb829b7d73ea6cc545
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-05 23:08:48 -0700

    coded the method to check the shortest path from all the 8 directions, 0 means i can travel, 1 means its a wall

--- src/ShortestPathInBinaryMatrix.java
+++ src/ShortestPathInBinaryMatrix.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 class Triplet{
     int x;
     int y;
@@ -12,6 +15,46 @@ class Triplet{
 
 
 public class ShortestPathInBinaryMatrix {
+    static int [] dx ={-1,-1,1,-1,0,1,1,0};
+    static int []dy = {-1,0,-1,1,1,1,0,-1};
+
+    static int shortestPathInBinaryMatrix(int grid[][]){
+        if(grid[0][0]==1){
+            return -1;
+        }
+        Queue<Triplet> q = new LinkedList<>();
+        Triplet tr = new Triplet(0,0,1);
+        q.add(tr);
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        while(!q.isEmpty()){
+            Triplet trip = q.remove();
+            int x = trip.x;
+            int y = trip.y;
+            int level= trip.level;
+
+            if(x==n-1 && y== m-1){
+                return level;
+            }
+            for(int k=0; k<8; k++){
+                int nx = x +dx[k];
+                int ny = y + dy[k];
+
+                if(nx<n && ny<m && nx>0 && ny>0 && grid[nx][ny]==0 ){
+                    grid[nx][ny]=1;
+                    Triplet ntr = new Triplet(nx,ny, level+1);
+                    q.add(ntr);
+                }
+            }
+        }
+            return -1;
+
+
+
+    }
+
 
     public static void main(String[] args) {
 
